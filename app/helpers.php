4c8a2e520e79e2282eb4b455971c3627eb4ee610
innovartech <software.innovar@gmail.com>
2026-01-25 23:01:23 -0300

    Revert "Merge pull request #18 from Innovartech-soft/codex/fix-redeclaration-error-for-active_class"
    This reverts commit bff345df585e7768de604ec53fd105ff37560381, reversing
    changes made to 4f23543193ee9aeac5023f2e754987ab3b967def.

--- app/helpers.php
+++ app/helpers.php
@@ -1,19 +1,13 @@
 <?php
   
-if (!function_exists('active_class')) {
-  function active_class($path, $active = 'active') {
-    return call_user_func_array('Request::is', (array)$path) ? $active : '';
-  }
+function active_class($path, $active = 'active') {
+  return call_user_func_array('Request::is', (array)$path) ? $active : '';
 }
 
-if (!function_exists('is_active_route')) {
-  function is_active_route($path) {
-    return call_user_func_array('Request::is', (array)$path) ? 'true' : 'false';
-  }
+function is_active_route($path) {
+  return call_user_func_array('Request::is', (array)$path) ? 'true' : 'false';
 }
 
-if (!function_exists('show_class')) {
-  function show_class($path) {
-    return call_user_func_array('Request::is', (array)$path) ? 'show' : '';
-  }
+function show_class($path) {
+  return call_user_func_array('Request::is', (array)$path) ? 'show' : '';
 }
